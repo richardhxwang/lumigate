7fafb1dcb5fe18911e1866cb210092901193daa6
whx <macmini@whxs-Mac-mini.local>
2026-03-21 23:16:23 +0800

    fix: selectedBacktestResult .value in script, strategy_name access

---
+++ lumitrade/frequi-custom/installed/assets/BotBalance-BkotPC2p.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./BotBalance.vue_vue_type_script_setup_true_lang-B_Jjk0ie.js";import"./index-CZ3qfAGa.js";import"./index-CZH3FGpE.js";import"./installCanvasRenderer-GKZFJXtg.js";import"./index-CS_4KwJW.js";import"./index-SdHQMw9A.js";import"./index-C7BOw2V6.js";import"./index-C0S1LAr1.js";export{o as default};
+//# sourceMappingURL=BotBalance-BkotPC2p.js.map
--- lumitrade/frequi-custom/installed/assets/BotBalance-qQ4CtkuB.js
+++
@@ -1,2 +0,0 @@
-import{_ as o}from"./BotBalance.vue_vue_type_script_setup_true_lang-BeVwz1KX.js";import"./index-BoxY0uyf.js";import"./index-vfsesnJV.js";import"./installCanvasRenderer-MLLnLrg8.js";import"./index-DCZZTRLy.js";import"./index-DFaDRJBT.js";import"./index-BS2zET7I.js";import"./index-DxcGkmCI.js";export{o as default};
-//# sourceMappingURL=BotBalance-qQ4CtkuB.js.map
