93a2aba1764c04954fd2bae450bff917979c3134
whx <macmini@whxs-Mac-mini.local>
2026-03-21 23:34:50 +0800

    fix: guard BacktestResultAnalysis computed against undefined props

---
+++ lumitrade/frequi-custom/installed/assets/BotBalance-BHhVmucG.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./BotBalance.vue_vue_type_script_setup_true_lang-D6B_PxEo.js";import"./index-fXlUfAQT.js";import"./index-w659AzGt.js";import"./installCanvasRenderer-Z1kih-zl.js";import"./index-DKF4Yktt.js";import"./index-D_Z3oUTs.js";import"./index-BkzA114M.js";import"./index-CR2M9dJn.js";export{o as default};
+//# sourceMappingURL=BotBalance-BHhVmucG.js.map
--- lumitrade/frequi-custom/installed/assets/HomeView-Dm2R7MDL.js
+++
@@ -1,2 +0,0 @@
-import{d as o,o as t,a,c as r,u as n}from"./index-BBmdWKvw.js";const _=o({__name:"HomeView",setup(s){const e=n();return t(()=>e.replace("/trade")),(c,u)=>(a(),r("div"))}});export{_ as default};
-//# sourceMappingURL=HomeView-Dm2R7MDL.js.map
