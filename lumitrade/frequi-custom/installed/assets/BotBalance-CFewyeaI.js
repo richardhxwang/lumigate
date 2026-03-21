59fa8e96b0d1a9cc773a3b4610082e5e5159e198
whx <macmini@whxs-Mac-mini.local>
2026-03-21 22:06:51 +0800

    fix: backtest auto-poll every 2s + load results on mount

--- lumitrade/frequi-custom/installed/assets/BotBalance-CFewyeaI.js
+++
@@ -1,2 +0,0 @@
-import{_ as o}from"./BotBalance.vue_vue_type_script_setup_true_lang-uHhXcmw8.js";import"./index-foQ6SExB.js";import"./index-DVdqhAZ-.js";import"./installCanvasRenderer-CXLefciZ.js";import"./index-Cb9zBDjd.js";import"./index-CsJz1zjo.js";import"./index-DVYJBR_H.js";import"./index-BhDDFhBF.js";export{o as default};
-//# sourceMappingURL=BotBalance-CFewyeaI.js.map
