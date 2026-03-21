b54bec4fd389dbd91c6a23309582658cce96a5c2
whx <macmini@whxs-Mac-mini.local>
2026-03-22 02:08:30 +0800

    fix: tradable_balance_ratio 1.0, PB sync metrics fixed, FinBERT healthy

--- lumitrade/frequi-custom/installed/assets/BotBalance-CKycHT3T.js
+++
@@ -1,2 +0,0 @@
-import{_ as o}from"./BotBalance.vue_vue_type_script_setup_true_lang-D46ptsdp.js";import"./index-lrm_R5Mx.js";import"./index-9Ha4uVX9.js";import"./installCanvasRenderer-BMUE_rdA.js";import"./index-D2iesHqy.js";import"./index-BQxJ43wc.js";import"./index-4qnV6Fre.js";import"./index-IVe4RQOs.js";export{o as default};
-//# sourceMappingURL=BotBalance-CKycHT3T.js.map
---
+++ lumitrade/frequi-custom/installed/assets/BotBalance-D25ZrD5m.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./BotBalance.vue_vue_type_script_setup_true_lang-DgNxXNUj.js";import"./index-D9ObOm2Q.js";import"./index-Dk6cYM_7.js";import"./installCanvasRenderer-CCMzCibJ.js";import"./index-Bzw10ZZn.js";import"./index-DYN0DvzA.js";import"./index-CVYnxoSG.js";import"./index-Dw9otAWq.js";export{o as default};
+//# sourceMappingURL=BotBalance-D25ZrD5m.js.map
