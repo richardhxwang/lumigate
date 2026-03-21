f1642739621f4a7d9df25eb13352cb1a19ae0867
whx <macmini@whxs-Mac-mini.local>
2026-03-21 22:51:47 +0800

    fix: backtest tab switch + strategy list via bt API + console logging

--- lumitrade/frequi-custom/installed/assets/BotBalance-DTbbUW4F.js
+++
@@ -1,2 +0,0 @@
-import{_ as o}from"./BotBalance.vue_vue_type_script_setup_true_lang-eMaD6Hdz.js";import"./index-BI8Jk3MJ.js";import"./index-DiVzly_2.js";import"./installCanvasRenderer-DuJ7GAb_.js";import"./index-UuofQa0O.js";import"./index-BSWNZKiB.js";import"./index-D3KHyk40.js";import"./index-BZ08xngW.js";export{o as default};
-//# sourceMappingURL=BotBalance-DTbbUW4F.js.map
