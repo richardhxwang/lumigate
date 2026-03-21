8dc37bcb3603c693077c3298f706ff58f72284e0
whx <macmini@whxs-Mac-mini.local>
2026-03-21 22:46:07 +0800

    fix: selectedBacktestResult unwraps strategy data to flat format

--- lumitrade/frequi-custom/installed/assets/BotBalance-BgQwI_Gf.js
+++
@@ -1,2 +0,0 @@
-import{_ as o}from"./BotBalance.vue_vue_type_script_setup_true_lang-DxWdAPiR.js";import"./index-uj9Q39Bu.js";import"./index-BgR2cKfX.js";import"./installCanvasRenderer-ag2SBffj.js";import"./index-BD_e49-9.js";import"./index-B2Gn6KAp.js";import"./index-DRAvMWLO.js";import"./index-DiCKApog.js";export{o as default};
-//# sourceMappingURL=BotBalance-BgQwI_Gf.js.map
