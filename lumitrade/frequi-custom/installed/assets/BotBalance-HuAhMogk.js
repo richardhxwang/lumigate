6b6f541435361a04e3e1dad31ab28be6ce4f581c
whx <macmini@whxs-Mac-mini.local>
2026-03-21 23:25:05 +0800

    debug: add console logging to selectedBacktestResult for format investigation

---
+++ lumitrade/frequi-custom/installed/assets/BotBalance-HuAhMogk.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./BotBalance.vue_vue_type_script_setup_true_lang-DidgK5lw.js";import"./index-BBmdWKvw.js";import"./index-CZhobuP6.js";import"./installCanvasRenderer-a2T1UDmz.js";import"./index-D1EwDhg7.js";import"./index-0FmEocXn.js";import"./index-NE64QNC-.js";import"./index-8nItU5p1.js";export{o as default};
+//# sourceMappingURL=BotBalance-HuAhMogk.js.map
