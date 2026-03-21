a052d6b0a3a4afbed24d0d9fa7261ea89ff11652
whx <macmini@whxs-Mac-mini.local>
2026-03-21 22:52:40 +0800

    fix: StrategySelect+FreqaiModelSelect use backtest API, no more 503

--- lumitrade/frequi-custom/installed/assets/BotBalance-CPZCwzen.js
+++
@@ -1,2 +0,0 @@
-import{_ as o}from"./BotBalance.vue_vue_type_script_setup_true_lang-CsKb5Iem.js";import"./index-rPObGkgH.js";import"./index-CbEUGyIe.js";import"./installCanvasRenderer-XmGQvm_2.js";import"./index-DFc_lrKX.js";import"./index-DUE_3mNV.js";import"./index-DNB90TIh.js";import"./index-DbSc6N7X.js";export{o as default};
-//# sourceMappingURL=BotBalance-CPZCwzen.js.map
