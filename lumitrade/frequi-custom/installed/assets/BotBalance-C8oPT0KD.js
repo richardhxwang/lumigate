83614012c655f0ba847def876cba75160019a763
whx <macmini@whxs-Mac-mini.local>
2026-03-21 23:43:47 +0800

    fix: wrap useBacktestApi() in reactive() for proper Vue template ref unwrapping

---
+++ lumitrade/frequi-custom/installed/assets/BotBalance-C8oPT0KD.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./BotBalance.vue_vue_type_script_setup_true_lang-lPrGUK-L.js";import"./index-BoW7o5CX.js";import"./index-l2-pNH0v.js";import"./installCanvasRenderer-Cr8iPwno.js";import"./index-DutH8yP9.js";import"./index-_QqlLEyE.js";import"./index-BqAu6ig8.js";import"./index-CrwJ5H-r.js";export{o as default};
+//# sourceMappingURL=BotBalance-C8oPT0KD.js.map
