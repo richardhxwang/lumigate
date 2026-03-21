92b0d4506bd4607ebd6f6330aca2abb1529ef3b0
whx <macmini@whxs-Mac-mini.local>
2026-03-21 22:32:56 +0800

    fix: backtest history uses computed wrapper for reliable reactivity

---
+++ lumitrade/frequi-custom/installed/assets/BotBalance-Ckr9SbGm.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./BotBalance.vue_vue_type_script_setup_true_lang-CY8HaUAd.js";import"./index-Cq8w2YJX.js";import"./index-CIARb9RB.js";import"./installCanvasRenderer-CgV8lIV1.js";import"./index-FyKxiMO2.js";import"./index-Bm4mD75d.js";import"./index-B74LbxzI.js";import"./index-DIzz9XaW.js";export{o as default};
+//# sourceMappingURL=BotBalance-Ckr9SbGm.js.map
---
+++ lumitrade/frequi-custom/installed/assets/HomeView-CFrfC2WL.js
@@ -0,0 +1,2 @@
+import{d as o,o as t,a,c as r,u as n}from"./index-Cq8w2YJX.js";const _=o({__name:"HomeView",setup(s){const e=n();return t(()=>e.replace("/trade")),(c,u)=>(a(),r("div"))}});export{_ as default};
+//# sourceMappingURL=HomeView-CFrfC2WL.js.map
