53ff3e0bdd6d93b2c89f19b38969b9005bc1c3a7
whx <macmini@whxs-Mac-mini.local>
2026-03-21 22:55:46 +0800

    fix: backtest API Basic Auth fallback when no token available

--- lumitrade/frequi-custom/installed/assets/BotBalance-CC7eSlv9.js
+++
@@ -1,2 +0,0 @@
-import{_ as o}from"./BotBalance.vue_vue_type_script_setup_true_lang-DlRIDU8n.js";import"./index-BNVORy6G.js";import"./index-B4r2K32C.js";import"./installCanvasRenderer-OebXMIL_.js";import"./index-C9GoZqT6.js";import"./index-B_DpIZU3.js";import"./index-DE6C4zWQ.js";import"./index-CV1ZgpCW.js";export{o as default};
-//# sourceMappingURL=BotBalance-CC7eSlv9.js.map
---
+++ lumitrade/frequi-custom/installed/assets/HomeView-Byuwni7d.js
@@ -0,0 +1,2 @@
+import{d as o,o as t,a,c as r,u as n}from"./index-BoxY0uyf.js";const _=o({__name:"HomeView",setup(s){const e=n();return t(()=>e.replace("/trade")),(c,u)=>(a(),r("div"))}});export{_ as default};
+//# sourceMappingURL=HomeView-Byuwni7d.js.map
