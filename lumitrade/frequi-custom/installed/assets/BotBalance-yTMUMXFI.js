6ee5ce5d40e3896c7a61ce55360845e33c94a629
whx <macmini@whxs-Mac-mini.local>
2026-03-22 11:28:30 +0800

    fix: auto-clear stale bot cache when bot count changes, rename LumiLearning

--- lumitrade/frequi-custom/installed/assets/BotBalance-yTMUMXFI.js
+++
@@ -1,2 +0,0 @@
-import{_ as o}from"./BotBalance.vue_vue_type_script_setup_true_lang-Rmo_JoKC.js";import"./index-CTYTKfSF.js";import"./index-g7vs0KRy.js";import"./installCanvasRenderer-CGrO9PgB.js";import"./index-OhHdpFwV.js";import"./index-BgxaRRHt.js";import"./index-BD5nna4Z.js";import"./index-CbFVPKtS.js";export{o as default};
-//# sourceMappingURL=BotBalance-yTMUMXFI.js.map
